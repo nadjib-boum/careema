70a8ee2f879507e50d03006caa00f563a065faa4
nadjib-boum <nadjib.dev22@gmail.com>
2025-06-15 02:40:59 +0400

    change dashboard second card number

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -47,7 +47,7 @@ export default async function DashboardPage() {
           <p className="text-3xl font-bold">{patientsCount}</p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow">
-          <h2 className="text-lg font-semibold mb-2">Pending Diagnoses</h2>
+          <h2 className="text-lg font-semibold mb-2">Total Diagnoses</h2>
           <p className="text-3xl font-bold">{diagnosisCount}</p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow">
